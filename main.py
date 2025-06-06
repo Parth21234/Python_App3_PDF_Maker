6d73036e789728f2fd9eb2cb93306cccf04404a3
Parth21234 <chauhanparth974@gmail.com>
2025-06-06 18:30:14 +0530

    Add footers.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation="P", unit='mm', format='A4')
+pdf.set_auto_page_break(auto=False, margin=0)
+# The page should not auto break.
 
 df = pd.read_csv("topics.csv")
 
@@ -15,8 +17,22 @@ for index, row in df.iterrows():
     pdf.line(10, 21, 200,21)
     # The arguments of the line method will be x1,y1,x2,y2.
 
+    # Set the footer.
+    pdf.ln(265) # Adding 265 break lines, each of 1 mm.
+
+    pdf.set_font(family="Times", style='I', size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align='R')
+
     for i in range(row["Pages"] - 1):
         pdf.add_page()
+
+        # Set the footer.
+        pdf.ln(277)  # Adding 277 break lines, each of 1 mm.
+
+        pdf.set_font(family="Times", style='I', size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=12, txt=row["Topic"], align='R')
 
 # border = 1 is for the box around the texts.
 # ln is actually a break line.
